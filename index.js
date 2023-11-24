bc4105c1751f312d9d465b3ef966d90bfc0d2e61
Elias Adjetey <eliasonics@gmail.com>
2023-11-24 01:50:49 +0000

    updated cicd-workflow

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ client.connect((err) => {
 
 const app = express()
 
-app.get('/', (req, res) => res.json('My API is working fine. #cicd workflow with Github Actions'))
+app.get('/', (req, res) => res.json('Updated: My API is working fine. #cicd workflow with Github Actions'))
 
 app.get('/users', async (req, res) => {
     const result = await client.query(`SELECT * FROM users`)
